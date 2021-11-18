4fffa2ad7d7ab56a1c7e0abaf067fc2cfbf37c90
filimda <filimda@yandex.ru>
2021-11-18 14:24:25 +0300

    Initial commit

---
+++ hospital/hospital/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&c*66axt-l$eg4jadps!7je#7v8v!py223(bj_zop##5ah+**z'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
